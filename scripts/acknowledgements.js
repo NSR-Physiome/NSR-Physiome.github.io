2e00b3c6a1dcf54cbcc42513d65a36f43a0a2e84
NSR-Physiome <barthj@uw.edu>
2020-07-09 10:01:15 -0700

    add acknowledgements

---
+++ scripts/acknowledgements.js
@@ -0,0 +1,8 @@
+
+// return acknowledgements
+function getAcknowledge() {
+
+var s = "JSim and Model development and archiving support are provided by the following grants: NIH U01HL122199 Analyzing the Cardiac Power Grid, 09/15/2015 - 05/31/2020, NIH/NIBIB BE08407 Software Integration, JSim and SBW 6/1/09-5/31/13; NIH/NHLBI T15 HL88516-01 Modeling for Heart, Lung and Blood: From Cell to Organ, 4/1/07-3/31/11; NSF BES-0506477 Adaptive Multi-Scale Model Simulation, 8/15/05-7/31/08; NIH/NHLBI R01 HL073598 Core 3: 3D Imaging and Computer Modeling of the Respiratory Tract, 9/1/04-8/31/09; as well as prior support from NIH/NCRR P41 RR01243 Simulation Resource in Circulatory Mass Transport and Exchange, 12/1/1980-11/30/01 and NIH/NIBIB R01 EB001973 JSim: A Simulation Analysis Platform, 3/1/02-2/28/07."
+
+return s;
+}
